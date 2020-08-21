4a3b8828a7127a30983452b65901325aefa8c1b2
Venkatesh Mandalapa <venkatesh.mandalapa@gmail.com>
2020-08-21 14:46:08 -0400

    bfs/dfs on an undirected graph

---
+++ bfs_undirected_graph.py
@@ -0,0 +1,25 @@
+import collections
+
+def bfs(graph, startnode):
+# Track the visited and unvisited nodes using queue
+        seen, queue = set([startnode]), collections.deque([startnode])
+        while queue:
+            vertex = queue.popleft()
+            print(vertex)
+            for node in graph[vertex]:
+                if node not in seen:        #checking if not visited
+                    seen.add(node)
+                    queue.append(node)
+
+
+
+# The graph dictionary
+adjacency = { 
+          "a" : set(["b","c"]),
+          "b" : set(["a", "d"]),
+          "c" : set(["a", "d"]),
+          "d" : set(["e"]),
+          "e" : set(["a"])
+        }
+
+bfs(adjacency, "a")
